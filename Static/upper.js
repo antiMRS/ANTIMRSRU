eb6fa1138feca99f32bdc4cf1c3e087290c372ca
antiMRS <mine2009_1p@inbox.ru>
2026-01-30 12:56:56 +0300

    Added buttons listeners

--- Static/upper.js
+++ Static/upper.js
@@ -8,3 +8,34 @@ upper.innerHTML = `
 upper.className = "header";
 
 document.body.append(upper);
+
+const themeToggle = document.getElementById("themeToggle");
+const body = document.body;
+
+//buttons
+const infBtn = document.getElementById("infBtn");
+const lnkBtn = document.getElementById("lnkBtn");
+const prjBtn = document.getElementById("prjBtn");
+const datBtn = document.getElementById("datBtn");
+
+infBtn.addEventListener("click", () => {
+    window.location.assign("https://antimrs.ru/Info");
+});
+lnkBtn.addEventListener("click", () => {
+    window.location.assign("https://antimrs.ru/Links");
+});
+prjBtn.addEventListener("click", () => {
+    window.location.assign("https://antimrs.ru/Projects");
+});
+datBtn.addEventListener("click", () => {
+    window.location.assign("https://antimrs.ru/Data");
+});
+themeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-theme");
+
+    if (body.classList.contains("dark-theme")) {
+        themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+    } else {
+        themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+    }
+});
